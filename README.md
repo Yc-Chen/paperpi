e3dad343c9267366536e83a81fd94e2b7c9606c3
Yicong Chen <omnitomato@gmail.com>
2018-04-02 13:09:51 +0200

    Initial commit
    initial commit
    
    added HATb model libraries
    
    adapted for python3
    
    new stuff

---
+++ README.md
@@ -0,0 +1,2 @@
+# paperpi
+A python based package to manage waveshare 7.5inch e-paper display.
---
+++ wavepy/main3.py
@@ -0,0 +1,69 @@
+##
+ #  @filename   :   main.cpp
+ #  @brief      :   7.5inch e-paper display demo
+ #  @author     :   Yehui from Waveshare
+ #
+ #  Copyright (C) Waveshare     July 28 2017
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documnetation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to  whom the Software is
+ # furished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ # THE SOFTWARE.
+ ##
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import epd7in5
+from logger import logger
+
+EPD_WIDTH = 640
+EPD_HEIGHT = 384
+
+def main():
+    epd = epd7in5.EPD()
+    epd.init()
+
+    # For simplicity, the arguments are explicit numerical coordinates
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
+    # draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
+    # draw the horizontal lines
+    draw.line((0, 0, 640, 0), fill=0, width=2)
+    draw.line((0, 128, 640, 128), fill=0, width=2)
+    draw.line((0, 256, 640, 256), fill=0, width=2)
+    draw.line((0, 381, 640, 381), fill=0, width=2)
+    # draw the vertical lines
+    draw.line((0, 0, 0, 384), fill=0, width=2)
+    draw.line((160, 0, 160, 384), fill=0, width=2)
+    draw.line((320, 0, 320, 384), fill=0, width=2)
+    draw.line((480, 0, 480, 384), fill=0, width=2)
+    draw.line((638, 0, 638, 384), fill=0, width=2)
+    # draw.arc((240, 120, 580, 220), 0, 360, fill = 255)
+    # draw.rectangle((0, 80, 160, 280), fill = 255)
+    # draw.arc((40, 80, 180, 220), 0, 360, fill = 0)
+    logger.debug('Going to send to screen')
+    image2 = Image.open('monocolor.bmp')
+    epd.display_frame(epd.get_frame_buffer(image2), epd.get_frame_buffer(image))
+    logger.debug('Frame displayed')
+    #
+    # image = Image.open('monocolor.bmp')
+    # epd.display_frame(epd.get_frame_buffer(image))
+
+if __name__ == '__main__':
+    main()
